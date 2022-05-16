cd50ff90e94495b9cd1c2468581c9ea8ab2aaf9b
sergiomc95 <sergiomartinc95@gmail.com>
2022-05-16 18:47:37 +0200

    Comienzo de P2 - Tranfiero archivos de P1

---
+++ P2/tienda/server-tienda.js
@@ -0,0 +1,66 @@
+//Servidor de la tienda
+
+const url = require('url');
+const http = require('http');
+const fs = require('fs');
+
+const PUERTO = 8080;
+
+const server = http.createServer((req, res) => {
+
+    console.log("Petición recibida")
+
+    const url = new URL(req.url, 'http://' + req.headers['host']);
+    console.log(url.pathname);
+
+    //Variable recurso
+    var resource = "";
+    //Analizar el recurso solicitado
+    if (url.pathname == '/') {
+      resource += "/tienda.html";
+    } else {
+      resource += url.pathname;
+    }
+
+    //Obtención del tipo de recurso solicitado
+    resource_type = resource.split(".")[1];
+    resource = "." + resource;
+
+    console.log("Recurso: " + resource);
+    console.log("Extensión: " + resource_type);
+
+    //Lectura asíncrona
+    fs.readFile(resource, function(err, data){
+
+      //Definición del tipo de archivo html.
+      var mime = "text/html"
+
+      //tipo de imágenes
+      if(resource_type == 'jpg' || resource_type == 'png' || resource_type == 'PNG' || resource_type == 'gif'){
+          mime = "image/" + resource_type;
+      }
+
+      //archivo css
+      if (resource_type == "css"){
+          mime = "text/css";
+      }
+
+      //Fichero no encontrado
+      if (err) {
+        resource = "./error.html";
+        data = fs.readFileSync(resource);
+        res.writeHead(404, {'Content-Type': mime});
+        console.log("404 Not Found");
+        res.write(data);
+        res.end();
+      }else{
+          res.writeHead(200, {'Content-Type': mime});
+          console.log("Peticion Recibida, 200 OK");
+          res.write(data);
+          res.end();
+      }
+    });
+});
+
+server.listen(PUERTO);
+console.log("Servidor de la tienda online escuchando en puerto: " + PUERTO)
